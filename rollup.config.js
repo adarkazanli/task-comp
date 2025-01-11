cc216ef94271c309801c1808d467f2aae60a1029
adarkazanli <adarkazanli@yahoo.com>
2025-01-10 20:27:39 -0600

    Update package and build configurations: rename package to @mpsinc/task-component, update version to 0.1.3, add postcss and babel plugins, and adjust rollup config for CSS handling. Enhance README with NPM publishing instructions.

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,11 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
+import babel from '@rollup/plugin-babel';
 
 export default {
-  input: 'src/components/TaskComponent.js',
+  input: 'src/index.js', // Changed this to point to index.js
   output: [
     {
       file: 'dist/index.js',
@@ -19,6 +21,18 @@ export default {
   ],
   plugins: [
     peerDepsExternal(),
+    postcss({
+      extensions: ['.css'],
+      minimize: true,
+      inject: {
+        insertAt: 'top'
+      }
+    }),
+    babel({
+      exclude: 'node_modules/**',
+      babelHelpers: 'bundled',
+      presets: ['@babel/preset-react']
+    }),
     resolve(),
     commonjs(),
     terser()
